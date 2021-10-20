86ad92e5dc856df66f6ba6121b1ab84affc602f7
Evgenii Terskikh <eterskikh@nvidia.com>
2021-10-20 17:41:00 +0300

    additional fixes

--- yatube/posts/validators.py
+++
@@ -1,7 +0,0 @@
-def validate_not_empty(value):
-    # Проверка "а заполнено ли поле?"
-    if value == '':
-        raise forms.ValidationError(
-            'Это поле обязательно для заполнения',
-            params={'value': value},
-        )
--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -28,42 +28,42 @@ urlpatterns = [
         'password_change/',
         PasswordChangeView.as_view(
             template_name='users/password_change_form.html'
-            ),
+        ),
         name='password_change_form'
     ),
     path(
         'password_change/done/',
         PasswordChangeDoneView.as_view(
             template_name='users/password_change_done.html'
-            ),
+        ),
         name='password_change_done'
     ),
     path(
         'password_reset/',
         PasswordResetView.as_view(
             template_name='users/password_reset_form.html'
-            ),
+        ),
         name='password_reset_form'
     ),
     path(
         'password_reset/done',
         PasswordResetDoneView.as_view(
             template_name='users/password_reset_done.html'
-            ),
+        ),
         name='password_reset_done'
     ),
     path(
         'reset/<uidb64>/<token>/',
         PasswordResetConfirmView.as_view(
             template_name='users/password_reset_confirm.html'
-            ),
+        ),
         name='password_reset_confirm'
     ),
     path(
         'reset/done/',
         PasswordResetCompleteView.as_view(
             template_name='users/password_reset_complete.html'
-            ),
+        ),
         name='password_reset_complete'
     ),
 ]
